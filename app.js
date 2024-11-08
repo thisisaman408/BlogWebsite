f202385e4e165d127cb9a2e6cd5f89b707350839
Aman Kumar <thisisaman408@gmail.com>
2024-11-08 18:02:14 +0530

    Just minor changes in app.js

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.post("/edit", (req, res) => {
       res.status(400).json({ success: false, message: "Error updating post" });
     });
 });
-
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log("Server started on port 3000");
 });
